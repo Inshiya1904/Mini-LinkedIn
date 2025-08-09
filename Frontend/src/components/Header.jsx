dde2b961d72fda902a1e6bdb64878bf072e9fc62
shahina Sheikh <sonashekhu09@gmail.com>
2025-08-09 15:53:29 +0530

    uodate

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
 
   const handleLogout = () => {
     logout();
-    window.location.href = "/login";
+    window.location.href = "/";
   };
 
   const toggleDropdown = () => {
